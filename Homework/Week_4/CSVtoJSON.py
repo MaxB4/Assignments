d44fd35a932fb46595c3178599fa97ed69157295
MaxB4 <max.baneke@hotmail.com>
2018-11-27 10:07:05 +0100

    added text and read JSON

---
+++ Homework/Week_4/CSVtoJSON.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# global standards
+INPUT ='LTRUCKSA.csv'
+
+# read input and put in dataframe
+df = pd.read_csv(INPUT)
+
+# turn into JSON file
+df.set_index("DATE").to_json('d3.json', orient='index')
+
