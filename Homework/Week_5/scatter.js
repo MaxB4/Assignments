b3ae0d71bf6129f6981c7b6152ae47669db6bb12
MaxB4 <max.baneke@hotmail.com>
2018-12-06 01:17:10 +0100

    final layout changes

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,3 +1,4 @@
+// load api datasets
 var womenInScience = "http://stats.oecd.org/SDMX-JSON/data/MSTI_PUB/TH_WRXRS.FRA+DEU+KOR+NLD+PRT+GBR/all?startTime=2007&endTime=2015"
 var consConf = "http://stats.oecd.org/SDMX-JSON/data/HH_DASH/FRA+DEU+KOR+NLD+PRT+GBR.COCONF.A/all?startTime=2007&endTime=2015"
 
@@ -9,35 +10,35 @@ window.onload = function () {
         womenInScience = transformResponse(response[0])
         consConf = transformResponse(response[1])
 
-        years_women = []
-        datapoint_women = []
-        len_womenInScience = 47
+        yearsWomen = []
+        datapointWomen = []
+        lenWomenInScience = 47
 
         // read first file and fill lists
         var i;
-        for (i = 0; i < len_womenInScience; i++) {
-            datapoint_women.push(womenInScience[i]["datapoint"])
-            years_women.push(womenInScience[i]["time"])
+        for (i = 0; i < lenWomenInScience; i++) {
+            datapointWomen.push(womenInScience[i]["datapoint"])
+            yearsWomen.push(womenInScience[i]["time"])
         }
-        
+
         // add None for missing datapoints
         var i;
         var listMissingValues = [10, 12, 14, 15, 17, 29, 30, 31]
         for (i = 0; i < (listMissingValues.length - 1); i++) {
-            datapoint_women.splice(listMissingValues[i], 0, "None")
+            datapointWomen.splice(listMissingValues[i], 0, "None")
         }
 
-        years_consConf = []
+        yearsConsConf = []
         country = []
-        datapoint_consConf = []
-        len_consConf = 54
+        datapointConsConf = []
+        lenConsConf = 54
 
         // add complete datapoints to lists
         var i;
-        for (i = 0; i < len_consConf; i++) {
-            years_consConf.push(consConf[i]["time"])
+        for (i = 0; i < lenConsConf; i++) {
+            yearsConsConf.push(consConf[i]["time"])
             country.push(consConf[i]["Country"])
-            datapoint_consConf.push(consConf[i]["datapoint"])
+            datapointConsConf.push(consConf[i]["datapoint"])
         }
         countryList = ["France", "Germany", "Korea", "Netherlands", "Portugal", "UK"]
         yearList = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
@@ -45,18 +46,18 @@ window.onload = function () {
         k = 0
         for (i = 0; i < countryList.length; i++) {
             for (j = 0; j < yearList.length; j++) {
-                if (datapoint_women[k] !== "None") {
-                    combinedList.push([datapoint_consConf[k], datapoint_women[k], countryList[i], yearList[j]]);
+                if (datapointWomen[k] !== "None") {
+                    combinedList.push([datapointConsConf[k], datapointWomen[k], countryList[i], yearList[j]]);
                 }
                 k++;
             }
         }
-
         // information text
         d3.select("body")
             .append("p").text("Student Name: Max Baneke")
             .append("p").text("Student Number: 10797564")
             .append("p").text("This scatterplot provides data on women in science and consumer confidence in different countries")
+            .append("p").text("Source: OECD")
 
         // global constants
         var margin = {
@@ -74,19 +75,19 @@ window.onload = function () {
         // number of datapoints to create
         var numbDataPoints = 47
 
-        // initialize scale
+        // initialize scales
         var xScale = d3.scaleLinear()
-            .domain([d3.min(datapoint_consConf, function (d) {
+            .domain([d3.min(datapointConsConf, function (d) {
                 return d;
-            }), d3.max(datapoint_consConf, function (d) {
+            }), d3.max(datapointConsConf, function (d) {
                 return d;
             })])
             .range([padding, w - padding * 2]);
 
         var yScale = d3.scaleLinear()
-            .domain([d3.min(datapoint_women, function (d) {
+            .domain([d3.min(datapointWomen, function (d) {
                 return d;
-            }), d3.max(datapoint_women, function (d) {
+            }), d3.max(datapointWomen, function (d) {
                 return d;
             })])
             .range([h - padding, padding]);
@@ -136,7 +137,8 @@ window.onload = function () {
         legend.append("text")
             .attr("x", 95)
             .attr("y", 9)
-            .attr("dy", ".35em")
+            .style("font-size", "12px")
+            .attr("dy", ".1em")
             .style("text-anchor", "end")
             .text(function (d) {
                 return d;
@@ -160,13 +162,11 @@ window.onload = function () {
 
         // create x axis labels
         svg.append("g")
-            //.attr("class", "axis")
             .attr("transform", "translate(0," + (h - padding) + ")")
             .call(xAxis);
 
         // create y axis labels
         svg.append("g")
-            //.attr("class", "axis")
             .attr("transform", "translate(" + padding + ",0)")
             .call(yAxis);
 
@@ -185,6 +185,15 @@ window.onload = function () {
             .attr("dy", "4em")
             .style("text-anchor", "middle")
             .text("Women in Science");
+
+        // plot title
+        svg.append("text")
+            .attr("x", (w / 2))             
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")  
+            .style("font-size", "14px") 
+            .style("text-decoration", "underline")  
+            .text("Women in Science and Consumer Confidence in 6 Different Countries");
 
     }).catch(function (error) {
         throw (error);
